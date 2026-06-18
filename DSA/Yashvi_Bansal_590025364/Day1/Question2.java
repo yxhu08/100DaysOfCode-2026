f66cb427d1c615693f85d9b3bc23f452232b494f
Yashvi Bansal <yashvibansal2308@gmail.com>
2026-06-19 00:28:43 +0530

    Create Question2.java

---
+++ DSA/Yashvi_Bansal_590025364/Day1/Question2.java
@@ -0,0 +1,11 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        int[] arr={23,45,37,45,45};
+        int sum=0;
+        for(int x:arr)
+            sum+=x;
+        System.out.println(sum);
+    }
+}
